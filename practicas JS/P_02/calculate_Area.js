4cd70ad3462bd02f345549bd469cc0075f668304
jovanni villa <Jovanni.villa35@gmail.com>
2025-09-24 00:36:19 -0600

    Practica 2
    se creo una calculadora de la base del rectangulo

---
+++ practicas JS/P_02/calculate_Area.js
@@ -0,0 +1,18 @@
+//funcion para calcular el area de un rectangulo
+function calculateArea(){
+    let base = parseFloat(document.getElementById("length").value);
+
+    let altura = parseFloat(document.getElementById("width").value);
+
+    return document.getElementById("result").innerHTML = base * altura;
+
+}
+
+//por fin lo puede lograr no se puede mandar parametros en el html
+
+
+
+
+
+
+
