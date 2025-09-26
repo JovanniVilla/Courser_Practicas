fcbd1fc8f51c310933ca747320b610bc593bbf55
JovanniVilla <jovanni.villa35@gmail.com>
2025-09-26 13:32:43 -0600

    Creacion de la practica 6
    se creo la practica 6
    para la captura de info de formularios y que esta se muestre en la pantalla

---
+++ practicas JS/P_05/product_survey.js
@@ -0,0 +1,44 @@
+function submitFeedback(){
+
+    // Obtener los valores de los campos del formulario
+    const name = document.getElementById("name").value;
+    const age = document.getElementById("age").value;
+    const email = document.getElementById("email").value;
+    const job = document.getElementById("job").value;
+    const designation = document.getElementById("designation").value;
+    const productType = document.getElementById("productType").value;
+    const feedback = document.getElementById("feedbackText").value;
+    const experience = document.getElementById("userExperience").value;
+
+    
+    // Validar que todos los campos estén completos
+    if (!name || !age || !email || !job || !designation || !productType || !feedback || !experience) {
+        alert("Por favor, completa todos los campos del formulario.");
+    }else {
+    alert('Gracias por tus comentarios, ' + name + '!');
+    };
+
+    //Mostrar la información del usuario en la página
+    document.getElementById("userName").innerHTML = name;
+    document.getElementById("userAge").innerHTML = age;
+    document.getElementById("userEmail").innerHTML = email;
+    document.getElementById("userJob").innerHTML = job;
+    document.getElementById("userDesignation").innerHTML = designation;
+    document.getElementById("userProductChoice").innerHTML = productType;
+    document.getElementById("userFeedback").innerHTML = feedback;
+    document.getElementById("userExperienceFeedback").innerHTML = experience;
+    document.getElementById("userInfo").style.display = "block";
+
+
+};
+// Captura el Botón de Envío en una variable
+const submitButton = document.getElementById("submitBtn");
+// Asigna la función submitFeedback al evento click del botón
+submitButton.onclick = submitFeedback;
+
+// Permitir enviar el formulario al presionar la tecla "Enter" 
+document.addEventListener('keydown', function(event) {
+   if (event.key === 'Enter') {
+     submitFeedback();
+   }
+ });
